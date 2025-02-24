11e7d016d9cc7a61116d0ee900335a82d77664f1
Oliver Keszöcze <olike@dtu.dk>
2025-02-24 11:19:26 +0100

    use custom tick method

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
 
-
+clkCounter = 0
 
 
 def myBin(val, minLen=3):
@@ -17,37 +17,45 @@ def myBin(val, minLen=3):
 
   return valS
 
+async def myTick(dut, n=1):
+    await ClockCycles(dut.clk,n)
+    clkCounter += n
 
-async def streaming_testcase(dut, width):
+async def streaming_testcase(dut, width, mul_select_bits, debug=False):
+        dut._log.info()
+
+        enable_bit = "1"
         # TODO compute the fill range?
         for x in range(0,10):
             for y in range(0,10):
                 xS = myBin(x,width)
                 yS = myBin(y,width)
                 pS = myBin(x*y,2*width)
-                dut._log.info(f"Testing {x}({xS}) * {y}({yS}) = {x*y}({pS})({width} bit, Streaming)")
-                for i in range (0,8):
-                    streamInInt = int("1" + xS[7-i] + yS[7-i],2)
+                dut._log.info(f"Testing {x}({xS}) * {y}({yS}) = {x*y}({pS})  ({width} bit, Streaming)")
+                for i in range (0,width):
+                    streamInIntString = mul_select_bits + enable_bit + xS[7-i] + yS[7-i]
+                    streamInInt = int(streamInIntString.lstrip('0'),2)
                     #dut._log.info(f"Setting to {streamInInt}")
                     dut.uio_in.value = streamInInt
-                    await ClockCycles(dut.clk,1)
-                    #clkCounter +=1
+                    await myTick(dut,1)
+
                 
                 dut.uio_in.value = 0
-                await ClockCycles(dut.clk,65)
-                #clkCounter+=65
+                await ClockCycles(dut.clk, width*width + 1)
+
                 
                 
                 for i in range(0,2*width):
                     outS = myBin(dut.uio_out.value,width)
-                    #dut._log.info(f"{clkCounter}: {outS}")
+                    dut._log.info(f"{clkCounter}: {outS}")
                     assert outS[0] == '1'
                     assert outS[1] == pS[((2*width)-1)-i]
-                    await ClockCycles(dut.clk,1)
-                    #clkCounter+=1
+                    await myTick(dut,1)
+
 
                 # wait just to ensure that we finished streaming
-                await ClockCycles(dut.clk,5)
+                await myTick(dut,5)
+
 
 @cocotb.test()
 async def test_project(dut):
@@ -65,14 +73,15 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await myTick(dut,10)
+    
     #clkCounter+=10
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    dut._log.info("Test SSMCl behavior")
 
-    await ClockCycles(dut.clk,1)
-    #clkCounter+=1
+    await myTick(dut,1)
+
 
     assert dut.uo_out.value == 0
 
@@ -98,7 +107,8 @@ async def test_project(dut):
             # idle a couple of clock cykles
             #await ClockCycles(dut.clk,4)
 
-    await streaming_testcase(dut,8)            
+    await streaming_testcase(dut,8, "1", "0")            
+    await streaming_testcase(dut,16, "1", "1")
 
 
 
